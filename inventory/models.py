b3951b632f727682d2487b1de8f2e6dc6d7fa1c7
eagraf <graf.ethan@gmail.com>
2017-09-07 01:02:34 -0400

    Made some basic models for the app.
    - Inventory model that just contains a name.
    - Computer model that contains computer info and foreign key to inventory.
    - Performed migration to update db.

--- inventory/models.py
+++ inventory/models.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Basic inventory model
+class Inventory(models.Model):
+    name = models.CharField(max_length=256)
+
+# Basic computer model
+class Computer(models.Model):
+    inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE)
+    manufacturer = models.CharField(max_length=256)
+    serial = models.CharField(max_length=256)
+    comments= models.CharField(max_length=1024)
