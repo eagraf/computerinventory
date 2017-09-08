2d78f4f9e1abde0f9cc783d57a4e153e1d84ac81
eagraf <graf.ethan@gmail.com>
2017-09-08 14:11:59 -0400

    Created form for adding a new inventory.
    - Url at /inventory/addinventory.
    - add_inventory view tests if request method is POST or GET, if POST it will
      create new inventory.

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'inventory'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<inventory_id>[0-9]+)/$', views.inventory, name='inventory'),
+    url(r'^addinventory/$', views.add_inventory, name='addinventory'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Inventory, Computer
 
@@ -14,7 +15,22 @@ def index(request):
 
 # Inventory page lists computers in a given inventory.
 def inventory(request, inventory_id):
-    name = Inventory.objects.get(pk=inventory_id).name
+    # First get the associated inventory.
+    try:
+        name = Inventory.objects.get(pk=inventory_id).name
+    except Inventory.DoesNotExist:
+        raise Http404("Inventory does not exist")
+
+    # Get and render the list of computers.
     computers = Computer.objects.filter(inventory=inventory_id)
     context = { 'name': name, 'computers': computers }
     return render(request, 'inventory/inventory.html', context)
+
+# Returns form for adding a new inventory.
+def add_inventory(request):
+    if request.method == 'GET':
+        return render(request, 'inventory/addinventory.html', {})
+    elif request.method == 'POST':
+        i = Inventory(name=request.POST['name'])
+        i.save();
+        return HttpResponseRedirect(reverse('inventory:index'))
