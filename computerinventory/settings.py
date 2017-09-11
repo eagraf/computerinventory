88966fe9bd19217c0a82a009347000189acb168f
eagraf <graf.ethan@gmail.com>
2017-09-10 20:36:23 -0400

    Worked on templating and styling.
    - Created a base template that is included by all other templates.
    - Included bootstrap 4.0 beta and applied it to some styles.
    - Worked on creating a static directory for css and other assets.
    - BUG: style.css is not loaded.

--- computerinventory/settings.py
+++ computerinventory/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'inventory.apps.InventoryConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'computerinventory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'inventory/templates/inventory')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,5 +118,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
